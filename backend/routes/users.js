e37fb3c7434b3605230bb75b6cf4ecfdfee08838
mjahles <ahlmatj@dunwoody.edu>
2020-04-10 12:47:46 -0500

    Created the offers.json file in the backend directory.  Changed the server response to respond with the data from offers.json when a user connects to the /users route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var offers = require('../offers.json');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json(offers);
 });
 
 module.exports = router;
+
+//We get to the offers.json data by navigating to the localhost:8080/users
+//We can see which path is the specified one by checking in the app.js file.
+//Another way we can figure out which path to use is by checking the filename.  For example, to view the offers.json data we use the /users route because the command to respond with the offers.json data is found in this file (users.js).
