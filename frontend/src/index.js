38ca301b400faf13ee92d6a7b0809d0c697a337e
mjahles <ahlmatj@dunwoody.edu>
2020-04-10 13:05:32 -0500

    Created Offer.js in the src directory and added in code that takes the data from offers.json (from the backend folder) and puts it into an array called state.  Added in an import statement to index.js in the src folder that imports the Offers.js component.  Replaced the contents of the ReactDom.render() so that it renders the Offers.js component.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Offers from './Offers';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Offers />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('offers')
 );
 
 // If you want your app to work offline and load faster, you can change
